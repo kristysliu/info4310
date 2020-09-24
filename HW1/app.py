c432375ad6188321e468dce6772a3ed5c38cbdfe
Kristy Liu <kl768@cornell.edu>
2020-09-24 11:26:23 -0400

    Other files

---
+++ HW1/app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask
+from whitenoise import WhiteNoise
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/',
+prefix='static/',index_file="index.html", autorefresh=True)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return make_response("Hello world")
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
